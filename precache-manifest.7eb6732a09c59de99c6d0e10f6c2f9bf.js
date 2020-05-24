e7bf82742d041f124a7a9cad25155f6b6bb30b79
Sudhanshu <vorasudhanshu@yahoo.co.in>
2020-05-24 17:23:22 +0200

    Updates

---
+++ precache-manifest.7eb6732a09c59de99c6d0e10f6c2f9bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25759f475d4ec0ea03c565a2cb889d9b",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "e28907d702059f2a6054",
+    "url": "/react-weather-app/static/css/main.4bf9126b.chunk.css"
+  },
+  {
+    "revision": "ec1d5d930689efb63f44",
+    "url": "/react-weather-app/static/js/2.28526c83.chunk.js"
+  },
+  {
+    "revision": "65e23a43b53f9d006780a8576c6a05a4",
+    "url": "/react-weather-app/static/js/2.28526c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28907d702059f2a6054",
+    "url": "/react-weather-app/static/js/main.8af2546a.chunk.js"
+  },
+  {
+    "revision": "5e86c695d3836a3aa3c7",
+    "url": "/react-weather-app/static/js/runtime-main.5416ceab.js"
+  }
+]);
